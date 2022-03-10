4e42531abc967c29b4bf133285009267246dcfc8
Dell <Dell@10.0.0.39>
2022-03-09 22:14:41 -0500

    I added password validator class.

---
+++ src/PasswordValidator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class PasswordValidator{ 
+        private String password;
+        public static boolean validPassword(String password){
+        boolean validPassword=true;
+            System.out.println("Passwords must have at least 8 characters");
+            System.out.println("Passwords must have at least one special character");
+            System.out.println("Please enter your desired password:"); 
+        int specialCharCount=0;
+            //iterate over each character to see if it is a special character
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                validPassword=true;
+            }
+            return validPassword;
+                
+    }
+    
+}
